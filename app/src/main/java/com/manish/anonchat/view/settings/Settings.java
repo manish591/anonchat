277618497cadda7c785df2ae6739294583b3adbc
Manish Devrani <manishdevrani777@gmail.com>
2024-12-31 01:46:33 +0530

    feat: added settings screen layout

---
+++ app/src/main/java/com/manish/anonchat/view/settings/Settings.java
@@ -0,0 +1,38 @@
+package com.manish.anonchat.view.settings;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.viewpager2.widget.ViewPager2;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.manish.anonchat.R;
+import com.manish.anonchat.databinding.FragmentSettingsBinding;
+
+public class Settings extends Fragment {
+    private FragmentSettingsBinding binding;
+
+    @Override
+    public View onCreateView(
+            @NonNull LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState
+    ) {
+
+        binding = FragmentSettingsBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+    }
+
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+}
